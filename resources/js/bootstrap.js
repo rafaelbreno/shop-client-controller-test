149de6369e7a25330748c52de0753293cde9a175
rafaelbreno <rafael_breno@outlook.com>
2020-10-08 19:25:54 -0300

    Added front-end dependencies for sass compilation

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -6,9 +6,19 @@ window._ = require('lodash');
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
+try {
+    window.Popper = require('popper.js').default;
+    window.$ = window.jQuery = require('jquery')
+
+    require('alpinejs')
+    require('bootstrap')
+} catch (e) {  }
+
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+window.axios.defaults.headers.common['X-CSRF-TOKEN'] = $('meta[name="csrf-token"]').attr('content');
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
